6b2996cb34bec7dc8f319d0f15baf41d9dad7c3d
Svenum <s.ziegler@holypenguin.net>
2024-01-19 17:29:58 +0100

    add label

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import {
   Tooltip,
   PointElement,
   LineElement,
+  Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 // Register ChartJS components using ChartJS.register
@@ -14,7 +15,8 @@ ChartJS.register(
   LinearScale,
   PointElement,
   LineElement,
-  Tooltip
+  Tooltip,
+  Legend
 );
 import style from "./style.module.css";
 import { ReactDOM } from "react";
@@ -23,11 +25,13 @@ const MyLineChart = () => {
   const data = require('../src/data.json');
   let burpeeDates:string[] = [];
   let burpeeNums:number[] = [];
+  let burpeeNumsTotal:number[] = [];
   let burpeeTotal:number = 0;
   
   data.burpees.forEach((burpee:any) => {
     burpeeDates.push(burpee.date);
-    burpeeNums.push(burpeeTotal + burpee.num);
+    burpeeNumsTotal.push(burpeeTotal + burpee.num);
+    burpeeNums.push(burpee.num);
     burpeeTotal += burpee.num;
   })
 
@@ -45,9 +49,16 @@ const MyLineChart = () => {
             labels: burpeeDates,
             datasets: [
               {
+                data: burpeeNumsTotal,
+                backgroundColor: "red", 
+                borderColor: "red",
+                label: "gesamt",
+              },
+              {
                 data: burpeeNums,
                 backgroundColor: "white", 
                 borderColor: "white",
+                label: "pro Tag",
               },
             ],
           }}
