f2ed97614f570392d286ba8882937a97ba75f075
Svenum <s.ziegler@holypenguin.net>
2024-01-24 21:49:35 +0100

    add data

--- app/page.tsx
+++ app/page.tsx
@@ -19,36 +19,31 @@ ChartJS.register(
   Legend
 );
 import style from "./style.module.css";
-import { ReactDOM } from "react";
+import { useState } from "react";
 
-const MyLineChart = () => {
+export default function Page() {
+  const buttonStyle = "bg-transparent text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-white rounded mr-2"
+  const buttonStyleSelected = "bg-transparent text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-white rounded mr-2"
+  
+  const [button2022, setButton2022] = useState(buttonStyle);
+  const [button2023, setButton2023] = useState(buttonStyle);
+  const [button2024, setButton2024] = useState(buttonStyle);
+
+  const year = "2024";
   const data = require('../src/data.json');
   let burpeeDates:string[] = [];
   let burpeeNums:number[] = [];
   let burpeeNumsTotal:number[] = [];
   let burpeeTotal:number = 0;
   
-  data.burpees.forEach((burpee:any) => {
+  data.burpees[year].forEach((burpee:any) => {
     burpeeDates.push(burpee.date);
     burpeeNumsTotal.push(burpeeTotal + burpee.num);
     burpeeNums.push(burpee.num);
     burpeeTotal += burpee.num;
   })
 
-  // Chart config
-  ChartJS.defaults.font.size = 15;
-
-  return (
-  <>
-    <div className={style.container}>
-      <h1 className={style.header}>
-        INSGESAMT WURDEN SCHON <br/>
-        <b><u>{burpeeTotal}</u></b> BURPEES <br/>
-        GEMACHT
-      </h1>
-      <div>
-        <Line
-          data={{
+  const charData = {
             labels: burpeeDates,
             datasets: [
               {
@@ -64,8 +59,8 @@ const MyLineChart = () => {
                 label: "pro Tag",
               },
             ],
-          }}
-          options = {{
+          };
+  const charOptions = {
             plugins: {
               legend: {
                 labels: {
@@ -75,10 +70,23 @@ const MyLineChart = () => {
                 }
               }
             }
-          }}
-          height="60%"
-          width="60%"
-        />
+        }
+  // Chart config
+  ChartJS.defaults.font.size = 15;
+
+  return (
+  <>
+    <div className={style.container}>
+      <h1 className={style.header}>
+        INSGESAMT WURDEN SCHON <br/>
+        <b><u>{burpeeTotal}</u></b> BURPEES <br/>
+        GEMACHT
+      </h1>
+      <div>
+        <button class={button2022} onClick={setButton2022(buttonStyleSelected)} > 2022 </button>
+        <button class={button2023} onClick={setButton2023(buttonStyleSelected)} > 2023 </button>
+        <button class={button2024} onClick={setButton2024(buttonStyleSelected)} > 2024 </button>
+        <Line data={charData} options={charOptions} height="60%" width="60%"/>
       </div>
     </div>
     <div className={style.giturl}>
@@ -87,4 +95,3 @@ const MyLineChart = () => {
   </>
   );
 };
-export default MyLineChart;
