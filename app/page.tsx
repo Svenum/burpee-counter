34bb26c56700f4e54bc5c1f7f7fc900fd2fb432d
Svenum <s.ziegler@holypenguin.net>
2024-01-26 23:01:11 +0100

    add menu

--- app/page.tsx
+++ app/page.tsx
@@ -25,18 +25,14 @@ export default function Page() {
   const buttonStyle = "bg-transparent text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-white rounded mr-2"
   const buttonStyleSelected = "bg-transparent text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-white rounded mr-2"
   
-  const [button2022, setButton2022] = useState(buttonStyle);
-  const [button2023, setButton2023] = useState(buttonStyle);
-  const [button2024, setButton2024] = useState(buttonStyle);
-
   const year = "2024";
-  const data = require('../src/data.json');
+  const burpees = require('../src/data/' + year + '/burpees.json');
   let burpeeDates:string[] = [];
   let burpeeNums:number[] = [];
   let burpeeNumsTotal:number[] = [];
   let burpeeTotal:number = 0;
   
-  data.burpees[year].forEach((burpee:any) => {
+  burpees.burpees.forEach((burpee:any) => {
     burpeeDates.push(burpee.date);
     burpeeNumsTotal.push(burpeeTotal + burpee.num);
     burpeeNums.push(burpee.num);
@@ -78,14 +74,14 @@ export default function Page() {
   <>
     <div className={style.container}>
       <h1 className={style.header}>
-        INSGESAMT WURDEN SCHON <br/>
+        INSGESAMT WURDEN {year} SCHON <br/>
         <b><u>{burpeeTotal}</u></b> BURPEES <br/>
         GEMACHT
       </h1>
       <div>
-        <button class={button2022} onClick={setButton2022(buttonStyleSelected)} > 2022 </button>
-        <button class={button2023} onClick={setButton2023(buttonStyleSelected)} > 2023 </button>
-        <button class={button2024} onClick={setButton2024(buttonStyleSelected)} > 2024 </button>
+        <button className={buttonStyle}> 2022 </button>
+        <button className={buttonStyle}> 2023 </button>
+        <button className={buttonStyle}> 2024 </button>
         <Line data={charData} options={charOptions} height="60%" width="60%"/>
       </div>
     </div>
