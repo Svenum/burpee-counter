631dfb5ebc6b897bfcc43493fba35f8c4730d679
Svenum <s.ziegler@holypenguin.net>
2024-01-27 12:34:29 +0100

    add Buch

--- app/schwarzes-buch/page.tsx
+++ app/schwarzes-buch/page.tsx
@@ -8,21 +8,24 @@ type Workouts = {
 export default function Page() {
   const workouts = require("../../src/data/black_book.json");
 
+  // Style
+  const cellStyle = "border-solid border-white border-2 p-4 text-xl";
+
   console.log(workouts)
   return(
     <>
       <div className="h-fit max-lg:w-fit lg:w-1/3 mx-auto">
-        <h1 className="text-4xl text-[#ff0000]">Diese Seite ist noch in Arbiet!!!</h1>
-        <table className="m-10">
+        <h1 className="text-4xl text-[#ff0000] m-auto font-antihero text-center">Scharzes Buch</h1>
+        <table className="m-auto mt-10">
           <tr>
-            <th>Workout</th>
-            <th>Beschreibung</th>
+            <th className={cellStyle}>Workout</th>
+            <th className={cellStyle}>Beschreibung</th>
           </tr>
           {
             workouts.map((item:Workouts) => (
               <tr key={item.name}>
-                <td>{item.name}</td>
-                <td><pre>{item.description}</pre></td>
+                <td className={cellStyle + " text-[#ff0000] text-center"}><b>{item.name}</b></td>
+                <td className={cellStyle}><pre>{item.description}</pre></td>
               </tr>
             ))
           }
